c37d22b8c57db59b2ba79e207f02b4269ef793f0
Daniel Onyeabo <daniel.onyeabo@gmail.com>
2022-09-30 17:20:13 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define UNUSED(x) (void)(x)
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
- */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
+* main - adds numbers
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+*
+* Return: 0 if no errors, else 1
+*/
 
-	if (argc > 1)
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (StringCheck(argv[i]))
+			if (isdigit(argv[i][j]) == 0)
 			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		printf("%d\n", result);
-		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-		return (1);
+		a += atoi(argv[i]);
 	}
-
+	printf("%d\n", a);
+	return (0);
 }
