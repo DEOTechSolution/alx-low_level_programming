51906c1c75c4ccd611c5d5a782564bb4517d0d16
Daniel Onyeabo <daniel.onyeabo@gmail.com>
2022-10-06 21:25:20 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,16 +3,16 @@
 #include "main.h"
 
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
-int main(int argc, char *argv[])
+ * main - multiply 2 numbers as arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 success
+ */
+  	int main(int argc, char *argv[])
 {
 
-unsigned long mul;
-int i, j;
+	unsigned long mul;
+	int i, j;
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
@@ -28,5 +28,5 @@ int i, j;
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
+	return (0);
 }
